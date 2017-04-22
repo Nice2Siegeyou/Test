56dada5281db8a75e1b3c97b470f64dbe7b67a99
Nice2Siegeyou <fake@hotmail.com>
2017-04-22 21:48:02 +0100

    deleted prints again

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,8 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("wtf?!?!");
-            Console.WriteLine("wtf again!")
+
         }
     }
 }
