471716faafb5a48fca45421706eaef5f8477fc30
Nice2Siegeyou <fake@hotmail.com>
2017-04-22 21:41:25 +0100

    added another console print!

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("wtf?!?!");
+            Console.WriteLine("wtf again!")
         }
     }
 }
